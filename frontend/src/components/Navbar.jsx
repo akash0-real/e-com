8942a7a819b5647a818505a38276b515f5969832
akash <aayush45r@gmail.com>
2025-11-19 15:22:24 +0530

    fixed the issues

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -75,8 +75,8 @@ const Navbar = ({ onSelectCategory, onSearch }) => {
       <header>
         <nav className="navbar navbar-expand-lg fixed-top">
           <div className="container-fluid">
-            <a className="navbar-brand" href="https://telusko.com/">
-              Telusko
+            <a className="navbar-brand" href="https://github.com/akash0-real">
+              Akash
             </a>
             <button
               className="navbar-toggler"
